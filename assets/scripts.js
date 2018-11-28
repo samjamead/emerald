cf61059563ab19728d4df92f73031cf873e27723
Sam Mead <samjamead@gmail.com>
2018-11-28 22:29:16 +0000

    Add mobile menu

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,16 @@
 jQuery( document ).ready(function($){
   // Smoooooth scrolling
-  $('.smooth-scroll').click(function(){
+  $('.smooth-scroll').on("click", function(){
     $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top - 80
     }, 500);
     return false;
   });
+
+  var $hamburger = $('.hamburger');
+  $hamburger.on("click", function(){
+    $hamburger.toggleClass("is-active");
+    $('.navbar').toggleClass("menu-open");
+  });
+
 });
